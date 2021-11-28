93fa2171cb4301dc41d5703ec3edac78409db9e8
MenekseYuncu <69850190+MenekseYuncu@users.noreply.github.com>
2021-11-28 20:49:30 +0300

    insert_into

---
+++ insert_into/SQLQuery9.sql
@@ -0,0 +1,5 @@
+insert into Kitap values (9,'Ýnsan ne ile yaþar','Tolstoy',190,20,'2016-11-07','MaviBulut')
+select * from Kitap
+
+insert into Kitap(KitapNo,KitapAd,KitapSayfa,KitapTarih,KitapYayýnevi)
+values (21,'Suç ve Ceza',1050,'2016-02-22','Merkür')
